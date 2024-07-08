d95bfd01f33be28253076cc1ba046b69a0c75a58
cksdud7158 <cksdud7158@naver.com>
2024-07-08 21:52:45 +0900

    전역 filter 및 pipe 추가     - feature :

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import setupSwagger from "./config/swagger/setup-swagger";
+import { ValidationPipe } from "@nestjs/common";
+import { GlobalExceptionFilter } from "./config/filter/globalException";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 입력 데이터를 DTO로 변환
+      whitelist: true, // DTO에 정의되지 않은 속성은 필터링
+    }),
+  );
 
   // swagger 세팅
   setupSwagger(app);
