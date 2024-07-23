8e53e6889b3311a967b4577b7586c56c36e42cc9
cksdud7158 <cksdud7158@naver.com>
2024-07-24 00:55:04 +0900

    콘서트 예약 API 낙관적락 구현 및 동시성 테스트 케이스 작성

--- src/domain/service/reservation/reservation.service.ts
+++ src/domain/service/reservation/reservation.service.ts
@@ -6,6 +6,10 @@ import {
 } from "@app/domain/interface/repository/ticket.repository";
 import { EntityManager } from "typeorm";
 import { TicketEntity } from "@app/domain/entity/ticket.entity";
+import { UserEntity } from "@app/domain/entity/user.entity";
+import { ConcertEntity } from "@app/domain/entity/concert.entity";
+import { ConcertSeatEntity } from "@app/domain/entity/concert-seat.entity";
+import { ConcertScheduleEntity } from "@app/domain/entity/concert-schedule.entity";
 
 @Injectable()
 export class ReservationService {
@@ -22,21 +26,17 @@ export class ReservationService {
     _manager?: EntityManager,
   ): Promise<TicketEntity[]> {
     // 티켓 여러개 만들기
-    const ticketIds = await this.ticketRepository.insert(
-      userId,
-      concertId,
-      concertScheduleId,
-      seatIds,
-      _manager,
+    let ticketList = seatIds.map(
+      (seatId) =>
+        new TicketEntity({
+          user: new UserEntity({ id: userId }),
+          concert: new ConcertEntity({ id: concertId }),
+          seat: new ConcertSeatEntity({ id: seatId }),
+          schedule: new ConcertScheduleEntity({ id: concertScheduleId }),
+        }),
     );
 
-    // 해당 seat pending 처리
-
-    // 티켓 리스트 조회
-    const ticketList = await this.ticketRepository.findByIds(
-      ticketIds,
-      _manager,
-    );
+    ticketList = await this.ticketRepository.save(ticketList, _manager);
 
     return ticketList;
   }
@@ -44,10 +44,12 @@ export class ReservationService {
   async getTicketList(
     userId: number,
     ticketIds: number[],
+    _manager?: EntityManager,
   ): Promise<TicketEntity[]> {
     const ticketList = await this.ticketRepository.findByIdsAndUserId(
       userId,
       ticketIds,
+      _manager,
     );
 
     if (ticketList.length !== ticketIds.length) {
--- test/application/use-case/reservation/reserve-concert.use-case.integration.spec.ts
+++ test/application/use-case/reservation/reserve-concert.use-case.integration.spec.ts
@@ -1,161 +1,106 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { BadRequestException, INestApplication } from "@nestjs/common";
-import { AppModule } from "@app/app.module";
+import { TestingModule } from "@nestjs/testing";
 import { DataSource } from "typeorm";
 import { User } from "@app/infrastructure/entity/user.entity";
+import { ReserveConcertUseCase } from "@app/application/use-case/reservation/reserve-concert.use-case";
 import { Concert } from "@app/infrastructure/entity/concert.entity";
 import { ConcertSchedule } from "@app/infrastructure/entity/concert-schedule.entity";
 import { ConcertSeat } from "@app/infrastructure/entity/concert-seat.entity";
-import { Ticket } from "@app/infrastructure/entity/ticket.entity";
-import { ConcertService } from "@app/domain/service/concert/concert.service";
-import ConcertSeatStatus from "@app/domain/enum/concert-seat-status.enum";
-import { ReserveConcertUseCase } from "@app/application/use-case/reservation/reserve-concert.use-case";
+import ConcertScheduleStatus from "@app/domain/enum/concert-seat-status.enum";
+import TicketStatus from "@app/domain/enum/ticket-status.enum";
 
 describe("ReserveConcertUseCase", () => {
-  let app: INestApplication;
   let reserveConcertUseCase: ReserveConcertUseCase;
   let dataSource: DataSource;
-  let concertService: ConcertService;
   let user: User;
   let concert: Concert;
   let schedule: ConcertSchedule;
-  let seat: ConcertSeat;
+  let seat1: ConcertSeat;
+  let seat2: ConcertSeat;
+  let userList: User[];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const module: TestingModule = global.mockModule;
 
-    app = module.createNestApplication();
     reserveConcertUseCase = module.get<ReserveConcertUseCase>(
       ReserveConcertUseCase,
     );
     dataSource = module.get<DataSource>(DataSource);
-    concertService = module.get<ConcertService>(ConcertService);
 
-    await app.init();
-
-    // Initialize database and insert test data
-    const queryRunner = dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
+    // 데이터베이스 초기화 및 테스트 데이터 삽입
     try {
-      user = await queryRunner.manager.save(User, {
-        point: 0,
-      });
+      await dataSource.createEntityManager().transaction(async (manager) => {
+        user = await manager.save(User, { point: 2000 });
 
-      concert = await queryRunner.manager.save(Concert, {
-        name: "Test Concert",
-        creat_at: new Date(),
-        update_at: new Date(),
-      });
+        userList = await manager.save(
+          User,
+          Array.from({ length: 100 }).map((_) => ({})),
+        );
 
-      schedule = await queryRunner.manager.save(ConcertSchedule, {
-        date: new Date(),
-        creat_at: new Date(),
-        update_at: new Date(),
-        concert: concert,
-      });
+        concert = await manager.save(Concert, {
+          name: "Test Concert",
+        });
 
-      seat = await queryRunner.manager.save(ConcertSeat, {
-        creat_at: new Date(),
-        update_at: new Date(),
-        schedule: schedule,
-        status: ConcertSeatStatus.SALE,
-        price: 1000,
-        seat_number: 1,
-      });
+        schedule = await manager.save(ConcertSchedule, {
+          date: new Date(),
+          concert: concert,
+        });
 
-      await queryRunner.commitTransaction();
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
+        seat1 = await manager.save(ConcertSeat, {
+          schedule: schedule,
+          status: ConcertScheduleStatus.SALE,
+          price: 1000,
+          seat_number: 1,
+        });
+        seat2 = await manager.save(ConcertSeat, {
+          schedule: schedule,
+          status: ConcertScheduleStatus.SALE,
+          price: 1000,
+          seat_number: 2,
+        });
+      });
+    } catch (e) {
+      console.log("error::", e);
     }
   });
 
-  afterEach(async () => {
-    // Clear data
-    const queryRunner = dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-    try {
-      await queryRunner.manager.clear(Ticket);
-      await queryRunner.manager.clear(ConcertSeat);
-      await queryRunner.manager.clear(ConcertSchedule);
-      await queryRunner.manager.clear(Concert);
-      await queryRunner.manager.clear(User);
-      await queryRunner.commitTransaction();
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
-    }
+  describe("ReserveConcertUseCase 통합테스트", () => {
+    it("정상 예약", async () => {
+      // given
 
-    await app.close();
-  });
+      // when
 
-  describe("ReserveConcertUseCase Integration Test", () => {
-    it("예약 성공", async () => {
-      const userId = user.id;
-      const concertId = concert.id;
-      const concertScheduleId = schedule.id;
-      const seatIds = [seat.id];
-
-      const tickets = await reserveConcertUseCase.execute(
-        userId,
-        concertId,
-        concertScheduleId,
-        seatIds,
+      const res = await reserveConcertUseCase.execute(
+        user.id,
+        concert.id,
+        schedule.id,
+        [seat1.id, seat2.id],
       );
 
-      expect(tickets.length).toBeGreaterThan(0);
-      expect(tickets[0].seat.id).toBe(seat.id);
-      expect(tickets[0].user.id).toBe(user.id);
+      // then
+      expect(res).toHaveLength(2);
+      expect(res[0].status).toBe(TicketStatus.PENDING);
     });
 
-    it("좌석 이용 불가로 예약 실패", async () => {
-      const userId = user.id;
-      const concertId = concert.id;
-      const concertScheduleId = schedule.id;
-      const seatIds = [seat.id];
+    it("ReserveConcertUseCase 동시성 테스트(낙관적 락)", async () => {
+      // given
 
-      // Simulate seat not available for sale by mocking concertService.checkSaleSeat
-      jest.spyOn(concertService, "checkSaleSeat").mockImplementation(() => {
-        throw new BadRequestException("Seat not available for sale.");
+      const requests = userList.map((user) => {
+        return reserveConcertUseCase.execute(user.id, concert.id, schedule.id, [
+          seat1.id,
+          seat2.id,
+        ]);
       });
 
-      const res = reserveConcertUseCase.execute(
-        userId,
-        concertId,
-        concertScheduleId,
-        seatIds,
+      // when
+      const results = await Promise.allSettled(requests);
+
+      // then
+      expect(results.filter((res) => res.status === "fulfilled")).toHaveLength(
+        1,
       );
-
-      await expect(res).rejects.toThrow(
-        new BadRequestException("Seat not available for sale."),
+      expect(results.filter((res) => res.status === "rejected")).toHaveLength(
+        99,
       );
-    });
-
-    it("좌석이 Pending 상태여서 예약 실패", async () => {
-      const userId = user.id;
-      const concertId = concert.id;
-      const concertScheduleId = schedule.id;
-      const seatIds = [seat.id];
-
-      // Simulate seat is already pending
-      await concertService.changeStatus(seatIds, ConcertSeatStatus.PENDING);
-
-      const res = reserveConcertUseCase.execute(
-        userId,
-        concertId,
-        concertScheduleId,
-        seatIds,
-      );
-
-      await expect(res).rejects.toThrow(
-        new BadRequestException("이미 판매된 좌석입니다."),
-      );
-    });
+    }, 10000);
   });
 });
