916e6d1e19e01ff6d0236ded02b98ea80bb54d4f
cksdud7158 <cksdud7158@naver.com>
2024-07-24 22:11:43 +0900

    redis spin_lock 패턴 사용하여 동시성 처리

--- src/application/use-case/reservation/reserve-concert.use-case.ts
+++ src/application/use-case/reservation/reserve-concert.use-case.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { BadRequestException, Inject, Injectable } from "@nestjs/common";
 import { ReservationService } from "@app/domain/service/reservation/reservation.service";
 import { ConcertService } from "@app/domain/service/concert/concert.service";
 import ConcertScheduleStatus from "@app/domain/enum/concert-seat-status.enum";
 import { DataSource } from "typeorm";
 import { TicketEntity } from "@app/domain/entity/ticket.entity";
 import { LockService } from "@app/domain/service/redis/redis.service";
+import { delay } from "@app/common/util/util";
 
 @Injectable()
 export class ReserveConcertUseCase {
@@ -22,40 +23,56 @@ export class ReserveConcertUseCase {
     seatIds: number[],
   ): Promise<TicketEntity[]> {
     const key = `reserve-${concertId}-${concertScheduleId}-${seatIds}`;
-    // 락 획득
-    await this.lockService.acquireLock(key, key);
+    let repeatTime = 0;
 
-    const ticketList = await this.dataSource
-      .createEntityManager()
-      .transaction(async (manager) => {
-        // 판매 가능 여부 체크
-        const concertSeatList = await this.concertService.checkSaleSeat(
-          seatIds,
-          manager,
-        );
+    while (true) {
+      // 락 획득
+      const hasLock = await this.lockService.acquireLock(key, key);
 
-        // pending 으로 상태 변경
-        concertSeatList.forEach((seat) => {
-          seat.status = ConcertScheduleStatus.PENDING;
+      // 락 획득을 못하면
+      if (!hasLock) {
+        // 재시도 10번 넘으면 잘가
+        if (repeatTime > 10) {
+          throw new BadRequestException("예약 불가");
+        }
+        repeatTime++;
+        // 대기후 재시도
+        await delay(2000);
+        continue;
+      }
+
+      const ticketList = await this.dataSource
+        .createEntityManager()
+        .transaction(async (manager) => {
+          // 판매 가능 여부 체크
+          const concertSeatList = await this.concertService.checkSaleSeat(
+            seatIds,
+            manager,
+          );
+
+          // pending 으로 상태 변경
+          concertSeatList.forEach((seat) => {
+            seat.status = ConcertScheduleStatus.PENDING;
+          });
+
+          // Pending 상태로 변경 업데이트
+          await this.concertService.changeSeatStatus(concertSeatList, manager);
+
+          // 티켓 발행
+          const ticketList = await this.reservationService.makeTickets(
+            userId,
+            concertId,
+            concertScheduleId,
+            seatIds,
+            manager,
+          );
+
+          return ticketList;
         });
 
-        // Pending 상태로 변경 업데이트
-        await this.concertService.changeSeatStatus(concertSeatList, manager);
+      await this.lockService.releaseLock(key);
 
-        // 티켓 발행
-        const ticketList = await this.reservationService.makeTickets(
-          userId,
-          concertId,
-          concertScheduleId,
-          seatIds,
-          manager,
-        );
-
-        return ticketList;
-      });
-
-    await this.lockService.releaseLock(key);
-
-    return ticketList;
+      return ticketList;
+    }
   }
 }
